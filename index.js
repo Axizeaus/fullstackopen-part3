d280c35f95dfc5c2bc1686350b0a01c99b10c85c
Axizeaus <axizeaus@gmail.com>
2023-07-16 20:51:31 +0630

    exercise 3.1 and 3.2 finished

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (request, response) => {
-  response.send('<h2>this is working</h2>')
-})
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  let message = `
+  <h2>Phonebook has info for ${persons.length} people</h2>
+  <p> ${new Date()}
+  `
+  response.send(message);
+});
+
+app.get("/api/persons", (request, response) => {
+  console.log(request);
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
